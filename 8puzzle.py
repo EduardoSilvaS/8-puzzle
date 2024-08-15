a4fc5b881df14360da2159608464af225216a8bc
EduardoSilvaS <eduardoss2020.ve@gmail.com>
2024-08-15 14:12:27 -0300

    macaco

--- 8puzzle.py
+++ 8puzzle.py
@@ -13,3 +13,24 @@ WIN = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("8 Puzzle")
 background = Background(WIDTH, HEIGHT, pygame.image.load(os.path.join("assets", "background.jpg")), WIN)
 
+def main_game():
+    run = True
+    clock = pygame.time.Clock()
+
+    # functions that'll be used in the game ------------------------------------------------------------------------------------------
+    def isClosed(): # check if the game is closed
+        for event in pygame.event.get(): 
+            if event.type == pygame.QUIT:
+                return True
+        return False
+
+    while run:
+        clock.tick(120)
+        
+        if isClosed():
+            pygame.quit()
+            run = False
+
+        pygame.display.update()
+
+main_game()
