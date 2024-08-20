993a16ab3cd636489e9954f0aa97fda1abcc2b0d
EduardoSilvaS <eduardoss2020.ve@gmail.com>
2024-08-19 23:04:42 -0300

    feat: Add image click handling functionality

--- 8puzzle.py
+++ 8puzzle.py
@@ -16,7 +16,7 @@ bg = pygame.image.load(os.path.join("assets", "background.jpg"))
 def main_game():
     run = True
     clock = pygame.time.Clock()
-    WIN.blit(bg, (5,5))
+    # WIN.blit(bg, (5,5))
     
     img_matrix = [[pygame.image.load(os.path.join("assets", "Macaco01.png")), pygame.image.load(os.path.join("assets", "Macaco02.png")), pygame.image.load(os.path.join("assets", "Macaco03.png"))],
                   [pygame.image.load(os.path.join("assets", "Macaco04.png")), pygame.image.load(os.path.join("assets", "Macaco05.png")), pygame.image.load(os.path.join("assets", "Macaco06.png"))],
@@ -29,11 +29,42 @@ def main_game():
                 return True
         return False
 
+    # Function to check if a point is within a rectangle
+    def is_point_in_rect(point, rect):
+        px, py = point
+        rx, ry, rw, rh = rect
+        return rx <= px <= rx + rw and ry <= py <= ry + rh
+
+    # Function to draw the images on the screen and store their positions
+    def draw_images():
+        image_positions = []
+        for i in range(len(img_matrix)):
+            for j in range(len(img_matrix[i])):
+                if img_matrix[i][j] != 'x':
+                    pos = (j*100+5, i*100+5)
+                    WIN.blit(img_matrix[i][j], pos)
+                    image_positions.append((img_matrix[i][j], pos[0], pos[1], 100, 100))
+        return image_positions
+
+    # Function to handle mouse click events
+    def handle_click(image_positions):
+        mouse_pos = pygame.mouse.get_pos()
+        for img, x, y, w, h in image_positions:
+            if is_point_in_rect(mouse_pos, (x, y, w, h)):
+                print(f"Image at ({x}, {y}) clicked!")
+
+
     while run:
         clock.tick(120)
         
         if isClosed():
             run = False
+        
+        image_positions = draw_images()
+
+        for event in pygame.event.get():
+            if pygame.mouse.get_pressed()[0]:
+                handle_click(image_positions)
 
         pygame.display.update()
 
